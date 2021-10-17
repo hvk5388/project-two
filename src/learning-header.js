1c87e74872350344bbb3940dad75be2a0cde2c7a
ejk1220 <ejkennedy137@gmail.com>
2021-10-17 17:28:54 -0400

    plz work

---
+++ src/learning-header.js
@@ -0,0 +1,40 @@
+import { html, css } from 'lit';
+import { SimpleColors } from '@lrnwebcomponents/simple-colors/simple-colors.js';
+
+export class LearningHeader extends SimpleColors {
+  static get tag() {
+    return 'learning-header';
+  }
+
+  constructor() {
+    super();
+    this.accentColor = 'pink';
+    this.dark = false;
+    this.title = 'Stuff';
+  }
+
+  static get properties() {
+    return {
+      ...super.properties,
+      title: { type: String },
+    };
+  }
+
+  static get styles() {
+    return [
+      ...super.styles,
+      css`
+        :host {
+          display: block;
+          background-color: var (--simple-colors-default-theme-accent-1);
+          color: var ();
+        }
+      `,
+    ];
+  }
+
+  render() {
+    return html` <div><slot></slot></div>`;
+  }
+}
+window.customElements.define(LearningHeader.tag, LearningHeader);
